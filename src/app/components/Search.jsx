1fd7f99e3b5e7fbc57be60345b3dde62952263c3
snilth <suchada.nilth@gmail.com>
2025-03-02 17:24:45 +0700

    hot fix

--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const Search = () => {
   return (
-    <form class="max-w-md mx-auto">
+    <form className="max-w-2xl mx-auto">
       <label
-        for="default-search"
-        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        htmlFor="default-search"
+        className="mb-4 text-lg font-medium text-gray-900 sr-only dark:text-white"
       >
-        Search an event...
+        Search
       </label>
-      <div class="relative">
-        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
           <svg
-            class="w-4 h-4 text-gray-500 dark:text-gray-400"
+            className="w-6 h-6 text-gray-500 dark:text-gray-400"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -20,9 +20,9 @@ const Search = () => {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
             />
           </svg>
@@ -30,16 +30,10 @@ const Search = () => {
         <input
           type="search"
           id="default-search"
-          class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search Mockups, Logos..."
+          className="focus:outline-none block w-full p-6 pl-14 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search an event..."
           required
         />
-        <button
-          type="submit"
-          class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Search
-        </button>
       </div>
     </form>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 /* Components */
 import Carousel from "./components/Carousel";
+import Search from "./components/Search";
 
 export default function Home() {
   return (
@@ -18,44 +19,12 @@ export default function Home() {
           ></Image>
         </div>
 
-        <div className="font-bold text-4xl text-center text-[#333]">
+        {/* <div className="font-bold text-4xl text-center text-[#333]">
           DISCOVER EVENTS
-        </div>
-
-        <form className="max-w-2xl mx-auto">
-          <label
-            htmlFor="default-search"
-            className="mb-4 text-lg font-medium text-gray-900 sr-only dark:text-white"
-          >
-            Search
-          </label>
-          <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
-              <svg
-                className="w-6 h-6 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
-            </div>
-            <input
-              type="search"
-              id="default-search"
-              className="block w-full p-6 pl-14 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search an event..."
-              required
-            />
-          </div>
-        </form>
+        </div> */}
+        
+        {/* Search Component */}
+        {/* <Search /> */}
       </div>
     </>
   );
