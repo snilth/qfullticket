bf0afaecb3ebf3633d29cf0a3793ec28d17dcd6c
Sukhita Liaopatthanapong <sukhita5926@gmail.com>
2025-03-04 18:16:08 +0700

    style

--- src/app/components/Payment.jsx
+++ src/app/components/Payment.jsx
@@ -49,7 +49,7 @@ const Payment = () => {
         <div className="flex flex-col justify-start items-start w-full xl:w-2/3 space-y-4 md:space-y-6 xl:space-y-8">
           {/* Customer's Info */}
           <div className="flex flex-col justify-start items-start bg-white dark:bg-[#343434] px-4 py-4 md:py-6 md:p-6 xl:p-8 w-full shadow-lg rounded-lg">
-            <h3 className="font-semibold text-xl text-[#f5f5f5]">ข้อมูลสมาชิก</h3>
+            <h3 className="font-semibold text-xl text-[#f5f5f5]">Member Information</h3>
             <br />
             <div className="flex items-center space-x-4">
               <FaUserCircle className="text-5xl text-gray-800 dark:text-white" />
@@ -64,19 +64,19 @@ const Payment = () => {
 
             {/* Payment Methods */}
             <div className="mt-6 w-full">
-              <h3 className="font-semibold text-xl text-[#f5f5f5]">วิธีการชำระเงิน</h3>
+              <h3 className="font-semibold text-xl text-[#f5f5f5]">Payment Methods</h3>
               <ul className="space-y-4 mt-4">
                 <li className="flex items-center space-x-4">
                   <div className="w-8 h-8 bg-orange-600 rounded-full flex justify-center items-center">
                     <span className="text-white">Credit</span>
                   </div>
-                  <p className="text-lg text-[#555] line-through">ชำระเงินด้วยบัตรเครดิต / เดบิต</p>
+                  <p className="text-lg text-[#555] line-through">Pay with Credit/Debit Card</p>
                 </li>
                 <li className="flex items-center space-x-4">
                   <div className="w-8 h-8 bg-green-600 rounded-full flex justify-center items-center">
                     <span className="text-white">iBank</span>
                   </div>
-                  <p className="text-lg text-[#f5f5f5]">ชำระเงินผ่านบัญชีธนาคาร</p>
+                  <p className="text-lg text-[#f5f5f5]">Pay via Bank Account</p>
                 </li>
               </ul>
             </div>
@@ -84,14 +84,14 @@ const Payment = () => {
             {/* Pickup Methods */}
             <div className="mt-6 w-full">
               <h3 className="font-semibold text-xl py-[17px] text-[#f5f5f5]">
-                วิธีการรับบัตร
+              Ticket Delivery Methods
               </h3>
               <ul className="space-y-4">
                 <li className="flex items-center space-x-4">
                   <div className="w-8 h-8 bg-violet-600 rounded-full flex justify-center items-center">
                     <span className="text-white">Self</span>
                   </div>
-                  <p className="text-lg text-[#f5f5f5]">รับบัตรด้วยตัวเอง</p>
+                  <p className="text-lg text-[#f5f5f5]">Pick up the ticket in person</p>
                 </li>
               </ul>
             </div>
@@ -101,7 +101,7 @@ const Payment = () => {
         {/* Right Side (Payment Summary and Booking Details) */}
         <div className="flex flex-col justify-start items-start w-full xl:w-1/3 space-y-4 md:space-y-6 xl:space-y-8">
           <div className="text-[#333] bg-white p-6 shadow-lg rounded-lg w-full">
-            <h3 className="font-semibold text-xl">รายละเอียดการจอง</h3>
+            <h3 className="font-semibold text-xl">Booking Details</h3>
             <div>
               <p className="mt-3 text-lg">
               2025 TREASURE T5 UNIT ASIA TOUR : MOVE 
@@ -113,31 +113,31 @@ const Payment = () => {
 
             <br />
 
-            <h3 className="font-semibold text-xl">รอบการแสดง</h3>
+            <h3 className="font-semibold text-xl">Show Schedule</h3>
             <div className="flex justify-between font-bold text-xl mt-2">
               <span className="text-red-500">Sun 25 May 2025 18:00</span>
             </div>
 
             {/* Booking Details */}
             <div className="mt-6">
-              <p className="text-lg">ประเภท: VIP</p>
-              <p className="text-lg">โซนที่นั่ง: {bookingDetails?.zone}</p>
-              <p className="text-lg">ที่นั่ง: {bookingDetails?.seat}</p>
-              <p className="text-lg">จำนวน: 1 ที่นั่ง</p>
-              <p className="text-lg">ราคาบัตร: {bookingDetails?.price}</p>
-              <p className="text-lg">ค่าบริการในการจัดส่ง: 30.00</p>
-              <p className="text-lg">วิธีการรับบัตร: -</p>
-              <p className="text-lg">วิธีการชำระเงิน: -</p>
+              <p className="text-lg">Type: VIP</p>
+              <p className="text-lg">Seating Zone: {bookingDetails?.zone}</p>
+              <p className="text-lg">Seat: {bookingDetails?.seat}</p>
+              <p className="text-lg">Quantity: 1 seat</p>
+              <p className="text-lg">Ticket Price: {bookingDetails?.price}</p>
+              <p className="text-lg">Delivery Service Fee: 30.00</p>
+              <p className="text-lg">Ticket Delivery Method: -</p>
+              <p className="text-lg">Payment Methods: -</p>
             </div>
 
             <br />
 
-            <h3 className="font-semibold text-xl">จำนวนเงินที่ต้องชำระ</h3>
+            <h3 className="font-semibold text-xl">Amount Due</h3>
             <div className="flex justify-between font-bold text-xl mt-2">
               <span className="text-red-500">6,530.00</span>
             </div>
             <button className="p-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-200">
-              ชำระเงิน
+              Pay
             </button>
           </div>
         </div>
